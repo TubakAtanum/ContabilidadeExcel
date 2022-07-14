638ac14bd948d57e8c70ac74759835f0bb085182
Tubak Atanum <oliveira64jonas@gmal.com>
2022-07-14 12:28:46 -0300

    Contabilidade

--- app/src/main/java/com/example/myapplication/Activities/ActivityExtrato.java
+++ app/src/main/java/com/example/myapplication/Activities/ActivityExtrato.java
@@ -5,34 +5,29 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ListView;
 
 import com.example.myapplication.R;
 import com.example.myapplication.SQL.Dados;
-import com.example.myapplication.SQL.DataBaseHelper;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ActivityExtrato extends AppCompatActivity {
 
+    List<Dados> dados = new ArrayList<Dados>();
     public RecyclerView recyclerView;
-    public ListView listView;
+    private RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_extrato);
         recyclerView = findViewById(R.id.recyclerview);
-        listView = findViewById(R.id.listView);
+        recyclerView.hasFixedSize();
 
-        DataBaseHelper dbh = new DataBaseHelper(ActivityExtrato.this);
-        List<Dados> getAll = dbh.getAll();
-        ArrayAdapter adapter = new ArrayAdapter<Dados>(ActivityExtrato.this, android.R.layout.simple_list_item_1, getAll);
-        listView.setAdapter(adapter);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
     }
 
 }
